ce848b52768cf150a39ec55e06277c10892c0482
Deepender <deepender281190@gmail.com>
2016-01-18 00:27:28 +0530

    init file added

---
+++ run.py
@@ -0,0 +1,7 @@
+## this file take input from ib and then make vector and run the experiment
+##it takes care of filtering epsilon etx
+
+from q_learning.dqn_training import *
+from ib.ticker_data import *
+
+##function to filter files
