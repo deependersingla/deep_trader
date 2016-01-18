d23622078e45c84197fe5e76757e8fd3a7badafb
deepender <deepender281190@gmail.com>
2016-01-18 13:50:48 +0530

    ticker updated to give data

--- ib/ticker_data.py
+++ ib/ticker_data.py
@@ -1,18 +1,22 @@
 from ib.opt import ibConnection, message
 from ib.ext.Contract import Contract
 from time import sleep
+from ib.ext.TickType import TickType as tt
+import pdb
+
+def error_handler(msg):
+    print (msg)
+
 
 def my_callback_handler(msg):
-    inside_mkt_bid = ''
-    inside_mkt_ask = ''
-
-    if msg.field == 1:
-        inside_mkt_bid = msg.price
-        print 'bid', inside_mkt_bid
-    elif msg.field == 2:
-        inside_mkt_ask = msg.price
-        print 'ask', inside_mkt_ask
-
+	#for reference on this /home/deep/development/IbPy/ib/ext/TickType
+	#another class method tt.getField(msg.field)
+	if msg.field in [tt.BID,tt.ASK,tt.LAST,tt.HIGH,tt.LOW,tt.OPEN,tt.CLOSE]:
+		print tt.getField(msg.field), msg.price 
+	elif msg.field in [tt.BID_SIZE,tt.ASK_SIZE,tt.LAST_SIZE,tt.VOLUME,tt.AVG_VOLUME]:
+		print tt.getField(msg.field), msg.size 
+	elif msg.field in [tt,LAST_TIMESTAMP]:
+		print tt.getField(msg.field), msg.time
 
 tws = ibConnection()
 tws.register(my_callback_handler, message.tickSize, message.tickPrice)
@@ -25,7 +29,7 @@ c.m_exchange = "NSE"
 c.m_currency = "INR"
 
 tws.reqMktData(1,c,"",False)
-sleep(25)
+sleep(10000000)
 
 print 'All done'
 
