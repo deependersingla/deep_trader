25f5bc243231ed230dba7d5ed4dedfb72df36cee
deepender <deepender281190@gmail.com>
2016-06-21 15:49:19 +0530

    wip for making datasets

--- tensor-supervised/episodic_data.py
+++ tensor-supervised/episodic_data.py
@@ -21,18 +21,41 @@ episode = 10 #lenght of one episode
 data_array = []
 parent_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 raw_data_file  = os.path.join(parent_dir,'data/sorted_data.csv') 
+moving_average_number = 1000 #number of time interval for calculating moving average
 
 def prepare_data():
 	stock_data = genfromtxt(raw_data_file, delimiter=',', dtype=None, names=True)
-	average_dataset = stock_data[0:1000]
-	for data in stock_data[1000:]:
-
-		#this data
-		#last 10 data
-		#one week data
-		#take this vector and store this
-		#array can be split into episode
+	average_dataset = []
+	total_data = []
+	temp_episode = []
+	index = 0
+	for data in stock_data:
+		temp = [data[2], data[3], data[4], data[5], data[6]]
+		average_dataset.append(temp)
+		print(index)
+		if index > moving_average_number:
+			mean = find_average(average_dataset)
+			mean_array = average_dataset/mean
+			last_one_hour_average = find_average(mean_array[-60:])
+			last_one_day_average = find_average(mean_array[-300:])
+			last_one_week_average = mean_array
+			last_minute_data = average_dataset[-1]
+			average_dataset = average_dataset[1:]
+			vector = []
+			vector.extend(last_minute_data)
+			vector.extend(last_one_hour_average)
+			vector.extend(last_one_day_average)
+			vector.extend(last_one_week_average)
+			temp_episode.append(vector)
+			if index % 10 == 0:
+				total_data.append(temp_episode)
+				temp_episode = []
+		index += 1
+	pdb.set_trace();
 	print(5)
+
+def find_average(data):
+    return np.mean(data, axis=0)
 
 def make_standard_data(last_n_data, daily_gain):
     column = len(last_n_data[0])
