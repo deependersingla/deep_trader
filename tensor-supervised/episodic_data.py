9245837fbeb6bb6b2b2e60a6885c7bdce9a04dac
deepender <deepender281190@gmail.com>
2016-07-03 10:37:43 +0530

    Data typo fixed

--- tensor-supervised/episodic_data.py
+++ tensor-supervised/episodic_data.py
@@ -20,8 +20,9 @@ from six.moves.urllib import request
 episode = 10 #lenght of one episode
 data_array = []
 parent_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
-raw_data_file  = os.path.join(parent_dir,'data/sorted_data.csv') 
+raw_data_file  = os.path.join(parent_dir,'ib/20_ftr_csv/NIFTY50.csv') 
 moving_average_number = 1000 #number of time interval for calculating moving average
+#pdb.set_trace()
 
 def prepare_data():
 	stock_data = genfromtxt(raw_data_file, delimiter=',', dtype=None, names=True)
@@ -39,21 +40,21 @@ def prepare_data():
 			mean_array = average_dataset/mean
 			last_one_hour_average = find_average(mean_array[-60:])
 			last_one_day_average = find_average(mean_array[-300:])
-			last_one_week_average = mean
-			last_minute_data = average_dataset[-1]
+			last_3_day_average = find_average(mean_array[-900:]) #this might change
+			last_minute_data = mean_array[-1]
 			average_dataset = average_dataset[1:]
 			vector = []
 			vector.extend(last_minute_data)
 			vector.extend(last_one_hour_average)
 			vector.extend(last_one_day_average)
-			vector.extend(last_one_week_average)
+			vector.extend(last_3_day_average)
 			total_data.append(vector)
 			#temp_episode.append(vector)
 			#if index % 10 == 0:
 			#	total_data.append(temp_episode)
 			#	temp_episode = []
 		index += 1
-	with open("data_wo_vol.pkl", "wb") as myFile:
+	with open("data.pkl", "wb") as myFile:
 		six.moves.cPickle.dump(total_data, myFile, -1)
 	print("Done")
 
